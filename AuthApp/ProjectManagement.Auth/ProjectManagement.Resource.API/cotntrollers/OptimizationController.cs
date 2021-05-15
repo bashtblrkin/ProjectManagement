ec2d1c08dd3e97582736ef2bec37c057be1a10fc
bashtblrkin <vanya.baturin@list.ru>
2021-05-16 00:23:34 +0300

    add optimization module backend

---
+++ AuthApp/ProjectManagement.Auth/ProjectManagement.Resource.API/cotntrollers/OptimizationController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Resource.API.models;
+
+namespace ProjectManagement.Resource.API.cotntrollers
+{
+    [Route("api/optimization")]
+    [ApiController]
+    public class OptimizationController : ControllerBase
+    {
+        private string userId => User.Claims.Single(c => c.Type == ClaimTypes.NameIdentifier).Value;
+
+        public OptimizationController() {
+            
+        }
+
+        [Route("combinedfunction")]
+        [Authorize]
+        [HttpPost]
+        public IActionResult GetAnswerCombinedObjectiveFunction(Matrix matrix)
+        {
+            if (ModelState.IsValid)
+            {
+                double[] max = new double[matrix.width];
+                double[] min = new double[matrix.width];
+                double sumWeight = 0;
+                double[] answerArray = new double[matrix.height];
+                double minAns;
+                int answer = 0;
+                for (int i = 0; i < matrix.height; i++)
+                {
+                    answerArray[i] = 0;
+                }
+                for (int i = 0; i < matrix.width; i++)
+                {
+                    sumWeight += matrix.Weight[i];
+                }
+                for (int i = 0; i < matrix.width; i++)
+                {
+                    matrix.Weight[i] = matrix.Weight[i]/ sumWeight;
+                }
+                for (int i = 0; i < matrix.width; i++)
+                {
+                    for (int j = 0; j < matrix.height; j ++)
+                    {
+                        if (j == 0)
+                        {
+                            max[i] = matrix.Data[j][i];
+                            min[i] = matrix.Data[j][i];
+                            continue;
+                        }
+                        if (matrix.Data[j][i] > max[i])
+                        {
+                            max[i] = matrix.Data[j][i];
+                        }
+                        if (matrix.Data[j][i] < min[i])
+                        {
+                            min[i] = matrix.Data[j][i];
+                        }
+                    }
+                }
+                for (int i = 0; i < matrix.width; i++)
+                {
+                    for (int j = 0; j < matrix.height; j++)
+                    {
+                        matrix.Data[j][i] = (matrix.Data[j][i] - min[i]) / (max[i]-min[i]);
+                    }
+                }
+                for (int i = 0; i < matrix.height; i++)
+                {
+                    for (int j = 0; j < matrix.width; j++)
+                    {
+                        answerArray[i] += matrix.Data[i][j] * matrix.Direction[j] * matrix.Weight[j]; 
+                    }
+                }
+                minAns = answerArray[0];
+                answer = 0;
+                for (int i = 1; i < matrix.height; i++)
+                {
+                    if (answerArray[i] < minAns)
+                    {
+                        minAns = answerArray[i];
+                        answer = i;
+                    }
+                }
+                return Ok(answerArray);
+            }
+            return BadRequest();
+        }
+    }
+
+
+}
---
+++ AuthApp/ProjectManagement.Auth/ProjectManagement.Resource.API/models/Matrix.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Resource.API.models
+{
+    public class Matrix
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public double[] Weight { get; set; }
+        public double[] Direction { get; set; }
+        public double[][] Data { get; set; }
+    }
+}
